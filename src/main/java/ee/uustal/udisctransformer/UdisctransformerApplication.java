807a5b0a75c5e6a60936b74fab26a6d14367f31d
Cardo Uustal <cardo@onoffapp.com>
2020-06-08 18:49:56 +0300

    Initial commit

---
+++ src/main/java/ee/uustal/udisctransformer/UdisctransformerApplication.java
@@ -0,0 +1,13 @@
+package ee.uustal.udisctransformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdisctransformerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdisctransformerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/uustal/udisctransformer/UdisctransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.uustal.udisctransformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdisctransformerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
